a221158067fdcfb8eafa20bda1c445fe991d108d
Jonathan <jonathan.che@outlook.com>
2019-02-01 19:06:12 -0800

    search bar displays

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,44 +2,19 @@ import * as React from 'react';
 import { TextField, Button, withStyles } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 
-const styles = {
-  root: {
-    padding: '2px 4px',
-    display: 'flex',
-    alignItems: 'center',
-    width: 400,
-  },
-  input: {
-    marginLeft: 8,
-    flex: 1,
-  },
-  iconButton: {
-    padding: 10,
-  },
-  divider: {
-    width: 1,
-    height: 28,
-    margin: 4,
-  },
-};
-
-interface StyleProps {
-  root: object,
-  input: object,
-  iconButton: object,
-  divider: object,
-}
-
-const Search = ({ classes }: StyleProps) => {
+const Search = (props):any => {
   return (
-    <div>
-      <SearchIcon />
+    <div id="search-bar-container">
+      <SearchIcon id="search-icon" />
       <TextField
         fullWidth={true}
         placeholder="Search..."
+        id="search-bar"
+        autoFocus={true}
+        InputProps={{"disableUnderline": true}}
       />
     </div>
   );
 };
 
-export default withStyles(styles)(Search);
+export default Search;
