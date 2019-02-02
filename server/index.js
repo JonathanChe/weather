114564ad56b66c61bce729acd3a6cb063c9ddb04
Jonathan <jonathan.che@outlook.com>
2019-02-01 21:25:31 -0800

    removing unnecessary dependencies, clean up index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-require('dotenv').config({ path: '../.env' });
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -10,12 +9,11 @@ app.use(cors());
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.post('/getWeather', (req, res) => {
-  console.log('req', req.body)
   axios.get('https://www.metaweather.com/api/location/search/?query=san')
-  .then(response => res.json(response.data));
+    .then(response => res.json(response.data));
 });
 
 app.listen(3000, () => console.log('listening on port 3000'));
